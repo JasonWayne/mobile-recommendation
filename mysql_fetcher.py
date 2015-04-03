c54e7255c7f9bca90ec66fa3b60ddc6b03dd6b81
JasonWayne <wuwenjie0102@gmail.com>
2015-04-03 19:20:15 +0800

    day 2 and day 3, refactoring code

--- mysql_fetcher.py
+++ mysql_fetcher.py
@@ -2,12 +2,13 @@ import MySQLdb
 import time
 import csv
 
-output = csv.writer(file("output/users_filtered.csv", 'wb'))
 db = MySQLdb.connect("localhost", "root", "", "mobile_recommendation")
 cursor = db.cursor()
-# sql = """SELECT * FROM users WHERE user_id = 51816656"""
-sql = """SELECT * FROM users_filtered"""
+# output = csv.writer(file("output/users_filtered.csv", 'wb'))
+# sql = """SELECT * FROM users_filtered"""
 
+output = csv.writer(file('output/user-35254700.csv', 'wb'))
+sql = """SELECT * FROM users WHERE user_id = 35254700"""
 
 start_time = time.clock()
 cursor.execute(sql)
--- train.py
+++ train.py
@@ -6,76 +6,90 @@ import time
 
 start_time = time.clock()
 
-reader = csv.reader(file('output/train.csv', 'r'))
+def getXy(list_of_days, down_sample=37):
+    X = []
+    y = []
+        
+    for day in list_of_days:
+        reader_feature = csv.reader(file('output/features_%d.csv' % day, 'r'))
+        reader_result = csv.reader(file('output/y_%d.csv' % day, 'r'))
 
-X = []
-count = 0
-for row in reader:
-    if int(row[6]) == 0:
-        count += 1
-        if count % 450 == 0:
-            X.append(row[2:6])
-    else:
-        X.append(row[2:6])
-    
-y = []
-reader = csv.reader(file('output/train.csv', 'r'))
-count = 0
-for row in reader:
-    if int(row[6]) == 0:
-        count += 1
-        if count % 450 == 0:
-            y.append(row[6])
-    else:
-        y.append(int(row[6]))
+        count = 0
+        for feature, result in zip(reader_feature, reader_result):
+            result = int(result[2])
+            
+            if result == 0:
+                count += 1
+                if count % down_sample == 0:
+                    X.append(feature[2:])
+                    y.append(result)
+            else:
+                X.append(feature[2:])
+                y.append(result)
+    return X, y
+
+X, y = getXy([1218, 1205, 1129, 1123], 22)
 
 
-    
-clf = svm.SVC(kernel='rbf', C=1)
+clf = svm.SVC(kernel='rbf', C=0.9, gamma=13)
 clf.fit(X, y)
-# clf = sklearn.ensemble.RandomForestClassifier(1000)
+# clf = sklearn.ensemble.RandomForestClassifier(200)
 # clf.fit(X, y)
 
-reader = csv.reader(file('output/normalized_1216_1218.csv', 'rb'))
+reader_feature = csv.reader(file('output/features_1217.csv', 'rb'))
+reader_result = csv.reader(file('output/y_1217.csv', 'rb'))
+
 X = []
-for row in reader:
-    X.append(row[2:6])
+y = []
+for (a, b) in zip(reader_feature, reader_result):
+    X.append(a[2:])
+    y.append(int(b[2]))
+
 
 predict = clf.predict(X)
 
-reader = csv.reader(file('output/normalized_1216_1218.csv', 'rb'))
-writer = csv.writer(file('output/tianchi_mobile_recommendation_predict.csv', 'wb'))
 
+true_positive = 0
+false_positive = 0
+true_negative = 0
+false_negative = 0
+
+real_buy = csv.reader(file('output/real_buy_1217.csv', 'r'))
+buy_set = set()
+for row in real_buy:
+    buy_set.add(tuple(row))
+reader_result = csv.reader(file('output/y_1217.csv', 'r'))
+result_set = set()
+for result, row in zip(predict, reader_result):
+    if result:
+        result_set.add(tuple([row[0], row[1]]))
+    
+        
+true_positive = len(set.intersection(buy_set, result_set))
+false_positive = len(result_set) - true_positive
+false_negative = len(buy_set) - true_positive
+print true_positive, false_positive, false_negative, true_negative 
+precision = float(true_positive) / (true_positive + false_positive)
+recall = float(true_positive) / (true_positive + false_negative)
+f1 = 2 / (1 / precision + 1 / recall)
+print precision, recall, f1
+
+
+
+X = []
+reader = csv.reader(file('output/features_1219.csv', 'r'))
+for row in reader:
+    X.append(row[2:])
+
+predict = clf.predict(X)
+
+writer = csv.writer(file('output/tianchi_mobile_recommendation_predict.csv', 'w'))
+reader = csv.reader(file('output/features_1219.csv', 'r'))
+  
 writer.writerow(['user_id', 'item_id'])
 for (x, y) in zip(reader, predict):
     if int(y) == 1:
-        writer.writerow([x[0], x[1]])
-
-# true_positive = 0
-# false_positive = 0
-# true_negative = 0
-# false_negative = 0
-# total = 0
-# 
-# for (x, y) in zip(predict, y):
-#     print x,y
-#     total += 1
-#     x = int(x)
-#     y = int(y)
-#     if x == 1 and y == 1:
-#         true_positive += 1
-#     elif x == 1 and y == 0:
-#         false_positive += 1
-#     elif x == 0 and y == 1:
-#         false_negative += 1
-#     elif x == 0 and y == 0:
-#         true_negative += 1
-#     
-#         
-# print total, true_positive, false_positive, false_negative, true_negative 
-# precision = float(true_positive) / (true_positive + false_positive)
-# print precision 
+        writer.writerow([str(x[0]), str(x[1])])
 
 stop_time = time.clock()
 print stop_time - start_time
-        
